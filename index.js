ba79ece15c2239f992999df176a84a8f5a1d6f1a
linda wanjiku <lindashykooh@gmail.com>
2023-09-21 18:41:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply=num1*num2;
+
+const min=1
+let max1= 20
+let random= Math.floor( Math.random() * (max1-min+1) + min)
+console.log(random)
+
+let num3=19
+let num4=5
+let mod =(num3 % num4)
+
+let max=20
+
